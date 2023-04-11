bad86330ebf248bf8978b1f55f1213c2b68fd6a8
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-12 01:24:10 +0530

    Call uc 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HashTableAndBinaryTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to the Hash Table and Bst problem*****");
+            BinarySearchTree<int> obj = new BinarySearchTree<int>(256);
+            obj.Add(230);
+            obj.Add(270);
+            obj.Display();
+        }
+    }
+}
