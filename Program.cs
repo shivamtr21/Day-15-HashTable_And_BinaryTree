0ca960bdff45432dc1b4f6fbd167db262023d5e3
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-12 01:29:36 +0530

    call size and new node in uc2

--- Program.cs
+++ Program.cs
@@ -5,10 +5,22 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Hash Table and Bst problem*****");
-            BinarySearchTree<int> obj = new BinarySearchTree<int>(256);
-            obj.Add(230);
-            obj.Add(270);
+            BinarySearchTree<int> obj = new BinarySearchTree<int>(56);
+            obj.Add(30);
+            obj.Add(70);
+            obj.Add(22);
+            obj.Add(40);
+            obj.Add(60);
+            obj.Add(95);
+            obj.Add(11);
+            obj.Add(65);
+            obj.Add(3);
+            obj.Add(16);
+            obj.Add(63);
+            obj.Add(67);
             obj.Display();
+            obj.Size();
+
         }
     }
 }
