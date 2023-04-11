9a9e81327d49180af090b4bb45cf20e81c84711b
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-12 01:25:03 +0530

    Call uc 1

---
+++ BinarySearchTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBinaryTree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T Data { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+
+        public BinarySearchTree(T Data)
+        {
+            this.Data = Data;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        int leftcount = 0;
+        int rightcount = 0;
+
+        public void Add(T item)
+        {
+            T currentValue = this.Data;
+            if (currentValue.CompareTo(item) > 0)
+            {
+                if (LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                }
+                else { this.LeftTree.Add(item); }
+            }
+
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Add(item);
+            }
+        }
+
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftcount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.Data.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightcount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
